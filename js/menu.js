605b96eb7bc5104b8d4e262895342f6b89367efe
Victor Ambrozi <victorambrozi.dev@gmail.com>
2023-02-24 20:03:57 -0300

    add event outside click and close menu

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,37 @@
 export function controlMenu() {
-    const menuContainer = document.querySelector(".menu-hamburguer-container");
-    const menuHamburguer = document.querySelector(".menu-hamburguer");
-    const navItems = document.querySelector(".header-navigation-items");
+  const menuContainer = document.querySelector(".menu-hamburguer-container");
+  const menuHamburguer = document.querySelector(".menu-hamburguer");
+  const navItems = document.querySelector(".header-navigation-items");
+  const menuElements = [menuContainer, menuHamburguer]
 
-    function handleClick() {
-        //adicionar classe active para ativar o menu responsivo
-        menuHamburguer.classList.toggle("active");
-        navItems.classList.toggle("active");
+  function handleClick(event) {
+
+    //adicionar classe active para ativar o menu responsivo
+    menuHamburguer.classList.add("active");
+    navItems.classList.add("active");
+
+    // fechar menu quando clicar fora
+    outsideClick(menuElements, () => {
+      menuHamburguer.classList.remove("active");
+      navItems.classList.remove("active");
+    });
+  }
+
+  function outsideClick(element, callback) {
+    const html = document.documentElement;
+    const navigationField = document.querySelector(".header-navigation");
+
+    function handleOutsideClick(event) {
+
+      if (!navigationField.contains(event.target)) { 
+        // se clicar fora do campo de navegação, o valor retorna false, então eu executo a callback, removendo as classes
+        callback();
+        html.removeEventListener("click", handleOutsideClick)
+      }
     }
 
-    function handleMouseLeave(event) {
-        console.log(event)
-    }
-    
-    function handleMouseMove({target}) {
-        target.addEventListener("mousedown", handleMouseLeave); // fecha o menu quando o mouse sair da caixa
-    }
-    
-    menuContainer.addEventListener("click", handleClick);
-    navItems.addEventListener("mousemove", handleMouseMove); 
+    html.addEventListener("click", handleOutsideClick);
+  }
+
+  menuContainer.addEventListener("click", handleClick);
 }
