996b73ced5ffb7bb6bda0e342d4f2f6b6668e223
Victor Ambrozi <victorambrozi.dev@gmail.com>
2023-02-27 17:34:00 -0300

    bug fix menu hamburger

--- js/menu.js
+++ js/menu.js
@@ -2,31 +2,41 @@ export function controlMenu() {
   const menuContainer = document.querySelector(".menu-hamburguer-container");
   const menuHamburguer = document.querySelector(".menu-hamburguer");
   const navItems = document.querySelector(".header-navigation-items");
-  const menuElements = [menuContainer, menuHamburguer]
+  let toggle = false;
 
-  function handleClick(event) {
 
+  function handleClick() {
     //adicionar classe active para ativar o menu responsivo
-    menuHamburguer.classList.add("active");
-    navItems.classList.add("active");
-
-    // fechar menu quando clicar fora
-    outsideClick(menuElements, () => {
+    if(!toggle) {
+      menuHamburguer.classList.add("active");
+      navItems.classList.add("active");
+      toggle = true;
+    } else {
+      toggle = false;
       menuHamburguer.classList.remove("active");
       navItems.classList.remove("active");
+    }
+
+
+    // fechar menu quando clicar fora
+    outsideClick(() => {
+      menuHamburguer.classList.remove("active");
+      navItems.classList.remove("active");
+      toggle = false;
     });
   }
 
-  function outsideClick(element, callback) {
+  function outsideClick(callback) {
     const html = document.documentElement;
     const navigationField = document.querySelector(".header-navigation");
 
     function handleOutsideClick(event) {
-
-      if (!navigationField.contains(event.target)) { 
+      if (!navigationField.contains(event.target)) {
         // se clicar fora do campo de navegação, o valor retorna false, então eu executo a callback, removendo as classes
+        console.log('outside click', event.target)
+
         callback();
-        html.removeEventListener("click", handleOutsideClick)
+        html.removeEventListener("click", handleOutsideClick);
       }
     }
 
