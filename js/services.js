051098ff65a7005b2eda6a617797ae99241becdf
Victor Ambrozi <victorambrozi.dev@gmail.com>
2023-02-27 20:30:25 -0300

    change the content of the modal]

--- js/services.js
+++ js/services.js
@@ -2,8 +2,52 @@ const featuredWorks = document.querySelectorAll(".featured-works-item");
 const modal = document.querySelector("[data-modal]");
 const btnModalClose = document.querySelector("[data-modal='close']");
 
+const modalContent = {
+    one: {
+        title: "Designing Dashborads",
+        subtitle: {
+            date: "2020",
+            category: "Dashboard"
+        },
+        image: "./assets/dashboard.svg"
+    },
+    two: {
+        title: "Retrato Vibrante de 2020",
+        subtitle: {
+            date: "2018",
+            category: "Illustration"
+        },
+        image: "./assets/retrato.svg"
+    },
+    three: {
+        title: "Tipografia Gedai",
+        subtitle: {
+            date: "2018",
+            category: "Typography"
+        },
+        image: "./assets/tipografia.svg"
+    },
+}
+
 function handleClick(event) {
     event.preventDefault();
+
+    const modalTitle = document.querySelector('[data-modal="title"]');
+    const modalSubtitle = document.querySelector('[data-modal="subtitle"]');
+    const modalImage = document.querySelector('[data-modal="image"]');
+
+    featuredWorks.forEach(item => {
+        if (item.contains(event.target)) { // faz as modificações se o target estiver dentro do item
+            const { id } = item.dataset;
+            const content = modalContent[id];
+
+            modalTitle.textContent = content.title;
+            modalSubtitle.children[0].textContent = content.subtitle.date;
+            modalSubtitle.children[1].textContent = content.subtitle.category;
+            modalImage.src = content.image;
+        }
+    })
+    
     modal.classList.toggle("active");
 }
 
